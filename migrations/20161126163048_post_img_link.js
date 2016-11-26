f6257cacea637fcf228e207fc8522918f722821e
Matthew Bell <logicnotlogical@outlook.com>
2016-11-26 17:16:57 +0200

    Add image link to post

--- migrations/20161126163048_post_img_link.js
+++ migrations/20161126163048_post_img_link.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.table('post', function (table) {
+        table.string('img_url');
+    });
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.table('post', function (table) {
+        table.dropColumn('img_url');
+    })
 };
